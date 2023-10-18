316845d657f6179f6d7cc39ca4786d119653ee2b
AntePrkacin <ante.prkacin03@gmail.com>
2023-10-18 19:44:16 +0200

    Revert "Prvi Controller i integracijski test."
    This reverts commit f87e3a924e9b3c7c95c598bfaa5a148fa2137a88.

--- IzvorniKod/Backend/prijave/src/main/java/fer/proinz/prijave/controller/PrijaveController.java
+++ IzvorniKod/Backend/prijave/src/main/java/fer/proinz/prijave/controller/PrijaveController.java
@@ -1,14 +1,4 @@
 package fer.proinz.prijave.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class PrijaveController {
-
-    @GetMapping( "/hello")
-    public ResponseEntity<String> PrijavaHello() {
-        return ResponseEntity.ok("Prijavili ste prijavu!");
-    }
 }
--- IzvorniKod/Backend/prijave/src/main/java/fer/proinz/prijave/model/Prijava.java
+++ IzvorniKod/Backend/prijave/src/main/java/fer/proinz/prijave/model/Prijava.java
@@ -1,20 +1,8 @@
 package fer.proinz.prijave.model;
 
-import lombok.Builder;
 import lombok.Data;
-
-import java.sql.Timestamp;
+import lombok.Getter;
 
 @Data
-@Builder
 public class Prijava {
-
-    private String title;
-    private String description;
-    private double latitude;
-    private double longitude;
-    private Timestamp createTime;
-    private Timestamp updateTime;
-
-
 }

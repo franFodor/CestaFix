15c329d99bec74e02f8a0e8114285d5b532c859e
KolencLS <148322135+KolencLS@users.noreply.github.com>
2023-11-07 17:53:45 +0100

    Update Package.json

--- IzvorniKod/Frontend/cestafix_front_v0.2/src/setupProxy.js
+++ IzvorniKod/Frontend/cestafix_front_v0.2/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api', // Define the path you want to proxy
     createProxyMiddleware({
-      target: 'http://localhost:8080', // Specify the target URL where your API is running
+      target: 'http://0.0.0.0:8080', // Specify the target URL where your API is running
       changeOrigin: true,
     })
   );

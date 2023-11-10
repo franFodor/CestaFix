88f4d3e796dd61e53756e639a4e02cdf788940c3
KolencLS <148322135+KolencLS@users.noreply.github.com>
2023-11-10 15:42:29 +0100

    Login Api Call Fix

--- IzvorniKod/Frontend/cestafix_front_v0.2/src/Header.js
+++ IzvorniKod/Frontend/cestafix_front_v0.2/src/Header.js
@@ -75,8 +75,7 @@ const Header = () => {
     e.preventDefault();
 
     const formData = {
-      username: e.target.username.value,
-      email: e.target.email.value,
+      email: e.target.username.value,
       password: e.target.password.value,
       remember: e.target.remember.checked,
     };

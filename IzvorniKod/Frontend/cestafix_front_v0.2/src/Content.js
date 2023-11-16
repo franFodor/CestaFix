43cc9c5d94897ec02e243a551f92fbeb9722ccd4
KolencLS <148322135+KolencLS@users.noreply.github.com>
2023-11-16 14:10:36 +0100

    Uspješno Renderanje Svih Markera iz DB

--- IzvorniKod/Frontend/cestafix_front_v0.2/src/Content.js
+++ IzvorniKod/Frontend/cestafix_front_v0.2/src/Content.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useMapEvents } from 'react-leaflet/hooks';
 import 'leaflet/dist/leaflet.css';
@@ -7,88 +6,69 @@ import L from 'leaflet';
 import { NovaPrijava } from './createReport.js';
 
 const Content = () => {
-    
-    const [markers, setMarkers] = useState(initialiseMark());
 
+   const [markers, setMarkers] = useState([]);
+   const markerIcon = new L.Icon({
+       iconUrl: require("./images/R.png"),
+       iconSize: [35, 35],
+   });
 
-     async function initialiseMark(){
-        const dbMarkers = await fetchMarkers();
-        return dbMarkers.map((marker) => (
-            <Marker key={marker.problemId} position={marker.geocode} icon={markerIcon}>
-                <Popup>{marker.popup}</Popup>
-            </Marker>
-        ))
-    };
+   async function fetchMarkers() {
+       const response = await fetch('/api/problems/getAllProblems', {
+           method: 'GET',
+           headers: {
+               'Content-Type': 'application/json',
+           },
+       });
 
+       if (!response.ok) {
+           throw new Error('Network response was not OK');
+       }
 
+       const data = await response.json();
+       return data;
+   }
 
-    useEffect(() => {
-        const fetchAndPopulateMarkers = async () => {
-            const dbMarkers = await fetchMarkers();
-            dbMarkers.forEach((marker) => {
-                console.log("Dodajem Marker>", marker);
-            //putMarker({ geocode: [marker.latitude, marker.longitude], popup: "Placeholder prijava" });
-            });
-        };
-        fetchAndPopulateMarkers();
-    }, []);
+   useEffect(() => {
+       const fetchAndPopulateMarkers = async () => {
+           const dbMarkers = await fetchMarkers();
+           setMarkers(dbMarkers.map((marker) => ({
+               position: [marker.latitude, marker.longitude],
+               popup: marker.status,
+               icon: markerIcon
+           })));
+       };
 
+       fetchAndPopulateMarkers();
+   }, []);
 
-    async function fetchMarkers() {
-        return fetch('/api/problems/getAllProblems', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not OK');
-                }
-                return response.json();
-            })
-            .catch(error => {
-                console.error('There has been a problem with your fetch operation:', error);
-            });
+   function AddMarker() {
+       useMapEvents({
+           click(e) {
+               setMarkers([...markers, { position: [e.latlng.lat, e.latlng.lng], popup: "Placeholder prijava", icon: markerIcon }]);
+               NovaPrijava();
+           }
+       });
 
-    }
+       return null;
+   }
 
-    const markerIcon = new L.Icon({
-        iconUrl: require("./images/R.png"),
-        iconSize: [35, 35],
-    });
-
-
-    const putMarker = (newMarker) => {
-        setMarkers([...markers, newMarker]);
-        console.log("Markeri u PutMarker> ", markers);
-        NovaPrijava();
-    };
-
-    function AddMarker() {
-        useMapEvents({
-            click(e) {
-                putMarker({ geocode: [e.latlng.lat, e.latlng.lng], popup: "Placeholder prijava" });
-                NovaPrijava();
-            }
-        });
-        return null;
-    }
-
-
-
-    return (
-        <main className='flex-grow w-full'>
-            <MapContainer center={[45.812915, 15.975522]} zoom={13} className='w-full h-full'>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=jl7SF9AkX5d5T6Di7nm2"
-                />
-                <AddMarker />
-                
-            </MapContainer>
-        </main>
-    );
+   return (
+       <main className='flex-grow w-full'>
+           <MapContainer center={[45.812915, 15.975522]} zoom={13} className='w-full h-full'>
+               <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=jl7SF9AkX5d5T6Di7nm2"
+               />
+               {markers.map((marker, index) => (
+                  <Marker key={index} position={marker.position} icon={marker.icon}>
+                      <Popup>{marker.popup}</Popup>
+                  </Marker>
+               ))}
+               <AddMarker />
+           </MapContainer>
+       </main>
+   );
 }
 
 export default Content;

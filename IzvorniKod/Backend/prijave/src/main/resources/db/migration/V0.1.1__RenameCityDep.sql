3835bb22627a6b1042af3c599526283f42b04363
AntePrkacin <ante.prkacin03@gmail.com>
2023-12-12 13:18:58 +0100

    citydep --> citydept

--- IzvorniKod/Backend/prijave/src/main/resources/db/migration/V0.1.1__RenameCityDep.sql
+++ IzvorniKod/Backend/prijave/src/main/resources/db/migration/V0.1.1__RenameCityDep.sql
@@ -6,3 +6,12 @@ RENAME COLUMN citydep_id TO citydept_id;
 
 ALTER TABLE citydept
 RENAME COLUMN citydep_name TO citydept_name;
+
+ALTER TABLE citydepcategory
+RENAME TO citydeptcategory;
+
+ALTER TABLE citydeptcategory
+RENAME COLUMN citydep_id TO citydept_id;
+
+ALTER TABLE users
+RENAME COLUMN citydep_id TO citydept_id;

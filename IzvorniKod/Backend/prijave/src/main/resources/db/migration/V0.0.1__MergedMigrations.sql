ed1d312919acdee5e8bc4a86b60d003343c0ede2
AntePrkacin <ante.prkacin03@gmail.com>
2024-01-10 13:41:48 +0100

    Dodana metoda za grupiranje reporta

--- IzvorniKod/Backend/prijave/src/main/resources/db/migration/V0.0.1__MergedMigrations.sql
+++ IzvorniKod/Backend/prijave/src/main/resources/db/migration/V0.0.1__MergedMigrations.sql
@@ -1,3 +1,31 @@
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL DEFAULT 'USER',
+    citydept_id INT
+);
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE Reports (
+    report_id SERIAL PRIMARY KEY,
+    user_id INT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    address VARCHAR(255),
+    photo BYTEA,
+    report_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    business_id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    problem_id INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+
 CREATE TABLE Citydept (
     citydept_id SERIAL PRIMARY KEY,
     citydept_name VARCHAR(150) NOT NULL
@@ -25,33 +53,16 @@ CREATE TABLE CitydeptCategory (
     FOREIGN KEY(category_id) REFERENCES Category(category_id)
 );
 
-CREATE TABLE Users (
-    user_id SERIAL PRIMARY KEY,
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL DEFAULT 'USER',
-    citydept_id INT,
-    FOREIGN KEY(citydept_id) REFERENCES Citydept(citydept_id),
-    CONSTRAINT chkRoleCitydep CHECK((role = 'STAFF' AND citydept_id IS NOT NULL)
-                                    OR (role <> 'STAFF' AND citydept_id IS NULL))
-);
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE Reports (
-    report_id SERIAL PRIMARY KEY,
-    user_id INT,
-    title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    address VARCHAR(255),
-    photo BYTEA,
-    report_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(50) NOT NULL,
-    longitude DOUBLE PRECISION NOT NULL,
-    latitude DOUBLE PRECISION NOT NULL,
-    business_id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
-    problem_id INT,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY(problem_id) REFERENCES Problems(problem_id)
-);
+ALTER TABLE Users
+ADD CONSTRAINT citydeptFK FOREIGN KEY(citydept_id) REFERENCES Citydept(citydept_id);
+
+ALTER TABLE Users
+ADD CONSTRAINT chkRoleCitydep CHECK((role = 'STAFF' AND citydept_id IS NOT NULL)
+                                    OR (role <> 'STAFF' AND citydept_id IS NULL));
+
+ALTER TABLE Reports
+ADD CONSTRAINT problemFK FOREIGN KEY(problem_id) REFERENCES Problems(problem_id);
+
+ALTER TABLE users
+ADD CONSTRAINT unique_email UNIQUE(email);

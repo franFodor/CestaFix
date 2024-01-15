c3cbe43bf24c372aee176b2b18c22c9d1f22d89a
AntePrkacin <ante.prkacin03@gmail.com>
2024-01-15 10:24:42 +0100

    React je case sensitive

--- IzvorniKod/Frontend/cestafix_front_v0.2/src/myAccount/UpdateReportTable.js
+++ IzvorniKod/Frontend/cestafix_front_v0.2/src/myAccount/UpdateReportTable.js
@@ -10,7 +10,7 @@ const UpdateReportTable = () => {
         const loggedUser = JSON.parse(decodeURIComponent(Cookies.get("userInfo")));
         const token = Cookies.get("sessionToken");
         try {
-            const problemsData = await APIGetStaffProblems(token, loggedUser.userId);
+            const problemsData = await APIGetStaffProblems(loggedUser.cityDept.cityDeptId,token);
             setProblems(problemsData);
         } catch (error) {
             console.error('Error fetching problems:', error);

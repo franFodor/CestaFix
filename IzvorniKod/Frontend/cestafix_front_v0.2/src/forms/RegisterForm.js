6eabb3762c512eb026aa2491650b6b5683aad572
Fran Fodor <fodorkafran8@gmail.com>
2024-01-15 10:31:20 +0100

    dodan id v button submit u register

--- IzvorniKod/Frontend/cestafix_front_v0.2/src/forms/RegisterForm.js
+++ IzvorniKod/Frontend/cestafix_front_v0.2/src/forms/RegisterForm.js
@@ -80,7 +80,7 @@ const RegisterFormComponent = () => {
                     <input type="password" placeholder="Lozinka" name="password" id="passwordid" required />
                     <label htmlFor="password-repeat"><b>Ponovi Lozinku</b></label>
                     <input type="password" placeholder="Ponovi lozinku" name="password-repeat" id="repatpasswordid" required />
-                    <button type="submit" className="signupbtn">Registriraj se</button>
+                    <button type="submit" className="signupbtn" id="signupid">Registriraj se</button>
                 </form>
             <div className="registerFail"></div>
         </div>

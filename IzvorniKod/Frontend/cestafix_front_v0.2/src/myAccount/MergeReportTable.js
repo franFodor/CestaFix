dbbe188c4b7769a3c9dea1e1bbb07e0e6857309c
KolencLS <148322135+KolencLS@users.noreply.github.com>
2024-01-18 20:46:21 +0100

    Dodan Loading Gif kod Staff MyAccounta i dodan gumb za routanje na report u UpdateReportStatus

--- IzvorniKod/Frontend/cestafix_front_v0.2/src/myAccount/MergeReportTable.js
+++ IzvorniKod/Frontend/cestafix_front_v0.2/src/myAccount/MergeReportTable.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie'; 
-import { APIGetStaffProblems, APIMergeReports } from '../API.js'; 
+import Cookies from 'js-cookie';
+import { APIGetStaffProblems, APIMergeReports } from '../API.js';
 import './MergeReportTable.css'
+import loadingGif from "../images/loading.gif"
 
 const MergeReportTable = () => {
-  const [problems, setProblems] = useState([]);
+  const [problems, setProblems] = useState();
   const [selectedReports, setSelectedReports] = useState(new Set());
   const token = Cookies.get("sessionToken");
 
   async function fetchData() {
     const loggedUser = JSON.parse(decodeURIComponent(Cookies.get("userInfo")));
     try {
-      
-      const problemsData = await APIGetStaffProblems(loggedUser.cityDept.cityDeptId,token);
+
+      const problemsData = await APIGetStaffProblems(loggedUser.cityDept.cityDeptId, token);
       setProblems(problemsData);
     } catch (error) {
       console.error('Error fetching problems:', error);
@@ -21,7 +22,6 @@ const MergeReportTable = () => {
 
   useEffect(() => {
     fetchData();
-    // eslint-disable-next-line
   }, []);
 
   const toggleReportSelection = (reportId) => {
@@ -38,60 +38,65 @@ const MergeReportTable = () => {
 
   const handleMerge = (problemId) => {
     APIMergeReports(token, problemId, Array.from(selectedReports)).then(() => {
-        setSelectedReports(new Set());
-        fetchData();
-      }
+      setSelectedReports(new Set());
+      fetchData();
+    }
     );
 
   };
 
-  return (
-    <div className="problems-container">
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>ID Problema</th>
-            <th>Kategorija</th>
-            <th>Status</th>
-            <th>Naslov prijave</th>
-            <th>Opis prijave</th>
-            <th>Adresa prijave</th>
-            <th>Status prijave</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {problems.map(problem => (
-            <>
-              <tr key={problem.problemId} className="problem-row">
-                <td>
-                  <button onClick={() => handleMerge(problem.problemId)}>Spoji izabrano</button>
-                </td>
-                <td><b>{problem.problemId}</b></td>
-                <td><b>{problem.category.categoryName}</b></td>
-                <td><b>{problem.status}</b></td>
-                <td colSpan="4"></td>
-              </tr>
-              {problem.reports.map(report => (
-                <tr key={report.reportId} className={`report-row ${selectedReports.has(report.reportId) ? 'selected' : ''}`}>
-                  <td className="noborders" colSpan="4"></td>
-                  <td>{report.title}</td>
-                  <td>{report.description}</td>
-                  <td>{report.address}</td>
-                  <td>{report.status}</td>
+  return (<>
+    {problems && (
+      <div className="problems-container">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>ID Problema</th>
+              <th>Kategorija</th>
+              <th>Status</th>
+              <th>Naslov prijave</th>
+              <th>Opis prijave</th>
+              <th>Adresa prijave</th>
+              <th>Status prijave</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {problems.map(problem => (
+              <>
+                <tr key={problem.problemId} className="problem-row">
                   <td>
-                    <button onClick={() => toggleReportSelection(report.reportId)}>
-                      {selectedReports.has(report.reportId) ? 'Briši' : 'Označi'}
-                    </button>
+                    <button onClick={() => handleMerge(problem.problemId)}>Spoji izabrano</button>
                   </td>
+                  <td><b>{problem.problemId}</b></td>
+                  <td><b>{problem.category.categoryName}</b></td>
+                  <td><b>{problem.status}</b></td>
+                  <td colSpan="4"></td>
                 </tr>
-              ))}
-            </>
-          ))}
-        </tbody>
-      </table>
-    </div>
+                {problem.reports.map(report => (
+                  <tr key={report.reportId} className={`report-row ${selectedReports.has(report.reportId) ? 'selected' : ''}`}>
+                    <td className="noborders" colSpan="4"></td>
+                    <td>{report.title}</td>
+                    <td>{report.description}</td>
+                    <td>{report.address}</td>
+                    <td>{report.status}</td>
+                    <td>
+                      <button onClick={() => toggleReportSelection(report.reportId)}>
+                        {selectedReports.has(report.reportId) ? 'Briši' : 'Označi'}
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </>
+            ))}
+          </tbody>
+        </table>
+      </div>)}
+    {!problems && (<img src={loadingGif} alt="Loading..." style={{ width: '5%', height: 'auto' }} />)}
+
+
+  </>
   );
 };
 
--- IzvorniKod/Frontend/cestafix_front_v0.2/src/myAccount/UpdateReportTable.js
+++ IzvorniKod/Frontend/cestafix_front_v0.2/src/myAccount/UpdateReportTable.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { APIGetStaffProblems, APIUpdateProblemStatus } from "../API";
 import Cookies from "js-cookie";
+import loadingGif from "../images/loading.gif"
 
 
 const UpdateReportTable = () => {
-    const [problems, setProblems] = useState([]);
+    const [problems, setProblems] = useState();
     const [selectedStatuses, setSelectedStatuses] = useState({});
 
     async function fetchData() {
@@ -45,30 +46,39 @@ const UpdateReportTable = () => {
     }
 
     return (
-        <div className="w-full  overflow-auto flex">
-        <div className="flex flex-col flex-grow overflow-auto pl-4">
-        {problems.map((problem) => (
-        <div className="relative bg-white shadow-lg rounded-lg p-4 mb-4 w-full  overflow-visible" key={problem.problemId}>
-        <h2 className="text-2xl mb-2">{problem.reports[0].title}</h2>
-        {problem.reports.map((report) => (
-         <div className="mb-2 word-break-all" key={report.reportId}>
-           <p>{report.description}</p>
-           <p>{report.address}</p>
-         </div>
-        ))}
-        <select className="mt-2" style={{zIndex: 1}} value={selectedStatuses[problem.problemId] || problem.status} onChange={e => handleStatusChange(problem.problemId, e.target.value)}>
-         <option value="Čeka Obradu">Čeka Obradu</option>
-         <option value="U Obradi">U obradi</option>
-         <option value="Obrađeno">Obrađeno</option>
-        </select>
-        </div>
-        ))}
-        </div>
-        <div className="flex items-center justify-center mr-4">
-        <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block" onClick={commitChanges}>Potvrdi Odabrane Promjene!</button>
-        </div>
-        </div>
+        <>
+          {problems && (
+            <div className="w-full overflow-auto flex">
+              <div className="flex flex-col flex-grow overflow-auto pl-4">
+                {problems.map((problem) => (
+                  <div className="relative bg-white shadow-lg rounded-lg p-4 mb-4 w-full overflow-visible" key={problem.problemId}>
+                    <h2 className="text-2xl mb-2">{problem.reports[0].title}</h2>
+                    {problem.reports.map((report) => (
+                      <div className="mb-2 word-break-all" key={report.reportId}>
+                       <p>Opis Prijave:   <b>{report.description}</b></p> 
+                       <p>Adresa: {report.address}</p>
+                       <p>Jedinstveni Identifikator Prijave:   {report.businessId}</p>
+                       <button className="px-4 py-2 bg-green-500 text-white rounded"onClick={()=>{if(report.businessId !=="")window.location.href = `/prijava/${report.businessId}`}}>Prikaži prijavu na Karti</button>                      
+                        <br></br> <br></br>
+                      </div>
+                    ))}
+                    <select className="mt-2" style={{zIndex: 1}} value={selectedStatuses[problem.problemId] || problem.status} onChange={e => handleStatusChange(problem.problemId, e.target.value)}>
+                      <option value="Čeka Obradu">Čeka Obradu</option>
+                      <option value="U Obradi">U obradi</option>
+                      <option value="Obrađeno">Obrađeno</option>
+                    </select>
+                  </div>
+                ))}
+              </div>
+              <div className="flex items-center justify-center mr-4">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block" onClick={commitChanges}>Potvrdi Odabrane Promjene!</button>
+              </div>
+            </div>
+          )}
+          {!problems && (<img src={loadingGif} alt="Loading..." style={{ width: '5%', height: 'auto' }} />)}
+        </>
        );
+       
        
        
        
